4a583f5928fe2a4a555f3b0d11207f7b9df2b710
chcbaram <chcbaram@paran.com>
2020-07-11 11:28:47 +0900

    파워 서플라이 옵션 변경

--- stm32h7b3i_fw/src/common/def.h
+++ stm32h7b3i_fw/src/common/def.h
@@ -19,12 +19,16 @@
 
 
 
-#define _DEF_LED1         0
-#define _DEF_LED2         1
-#define _DEF_LED3         2
-#define _DEF_LED4         3
+#define _DEF_LED1                   0
+#define _DEF_LED2                   1
+#define _DEF_LED3                   2
+#define _DEF_LED4                   3
 
 
+#define _DEF_UART1                  0
+#define _DEF_UART2                  1
+#define _DEF_UART3                  2
+#define _DEF_UART4                  3
 
 
 
